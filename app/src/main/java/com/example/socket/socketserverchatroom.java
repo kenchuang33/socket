66d330eb1876ca581617d2510a3ba246a3c21a33
kenchuang33 <aa88101033@gmail.com>
2023-09-28 17:02:00 +0800

    test

--- app/src/main/java/com/example/socket/socketserverchatroom.java
+++ app/src/main/java/com/example/socket/socketserverchatroom.java
@@ -30,7 +30,9 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class socketserverchatroom extends AppCompatActivity {
     private Thread mThread = null;
@@ -39,12 +41,17 @@ public class socketserverchatroom extends AppCompatActivity {
     private int SERVER_PORT = 7100; //傳送埠號
     private String message;
     private BufferedReader reader;
+    String receivedData;
     private Socket socket;
     private String tmp;
     private PrintWriter out;
     private List<ClientHandler> clients = new ArrayList<>();
 
     private List<String> messages = new ArrayList<>();
+
+    private void removeClient(ClientHandler clientToRemove) {
+        clients.remove(clientToRemove);
+    }
 
 
     @Override
@@ -57,9 +64,10 @@ public class socketserverchatroom extends AppCompatActivity {
         tvMessages = findViewById(R.id.tvMessages);
         etMessage = findViewById(R.id.etMessage);
         Button btnSend = findViewById(R.id.btnSend);
+        Button btnLeave = findViewById(R.id.serverleave);
 
         Intent receivedIntent = getIntent();
-        String receivedData = receivedIntent.getStringExtra("servername");
+        receivedData = receivedIntent.getStringExtra("servername");
         socketserverNameAccept.setText("Hi  "+receivedData);
 
         try {
@@ -79,11 +87,25 @@ public class socketserverchatroom extends AppCompatActivity {
             public void onClick(View v) {
                 message = etMessage.getText().toString();
                 if (!message.isEmpty()) {
-                    new Thread(new SendData(message)).start();
+                    String fullmessage = receivedData + ": " + message;
+                    new Thread(new SendData(fullmessage)).start();
                 }
             }
         });
+        btnLeave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                message ="server已經離開請各自離開";
+                if (!message.isEmpty()) {
+                    String fullmessage = receivedData + ": " + message;
+                    new Thread(new SendData(fullmessage)).start();
+                }
+                finish();
+            }
+        });
     }
+
+
 
 
     class serverThread implements Runnable {
@@ -95,7 +117,7 @@ public class socketserverchatroom extends AppCompatActivity {
                 try {
                     while (true) {
                         socket = serverSocket.accept();
-
+                        Log.d("asd",socket.toString());
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
@@ -119,19 +141,91 @@ public class socketserverchatroom extends AppCompatActivity {
     }
 
 
+//    class ClientHandler implements Runnable {
+//        private Socket clientSocket;
+//        private BufferedReader reader;
+//        private PrintWriter out;
+//
+//
+//
+//        public ClientHandler(Socket socket) {
+//            clientSocket = socket;
+//            try {
+//                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//                out = new PrintWriter(new BufferedWriter(
+//                        new OutputStreamWriter(clientSocket.getOutputStream())), true);
+//
+//                // 发送保存的消息给新客户端
+//                for (String message : messages) {
+//                    out.println(message);
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        @Override
+//        public void run() {
+//            try {
+//                while (true) {
+//                    String tmp = reader.readLine();
+//                    if (tmp == null) {
+//                        reader.close();
+//                        out.close();
+//                        clientSocket.close();
+//                        removeClient(this); // 刪除客戶端
+//                        break; // 客户端断开连接
+//                    }
+//
+//                    // 解析用户名和消息文本
+//                    int colonIndex = tmp.indexOf(":");
+//                    if (colonIndex != -1) {
+//                        final String senderName = tmp.substring(0, colonIndex);
+//                        final String message = tmp.substring(colonIndex + 2); // 加 2 是为了跳过冒号和空格
+//
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                tvMessages.append("收 " + senderName + ": " + message + "\n");
+//                            }
+//                        });
+//
+//                        // 将消息保存到列表中
+//                        messages.add(tmp);
+//
+//                        // 将消息广播给其他客户端
+//                        for (ClientHandler client : clients) {
+//                            if (client != this) {
+//                                client.out.println(tmp);
+//                            }
+//                        }
+//                    }
+//                }
+//
+//                // 客户端断开连接，关闭相关资源
+//                reader.close();
+//                out.close();
+//                clientSocket.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
     class ClientHandler implements Runnable {
         private Socket clientSocket;
         private BufferedReader reader;
         private PrintWriter out;
+        private String clientIP; // 新增用於儲存客戶端 IP 的變數
 
         public ClientHandler(Socket socket) {
             clientSocket = socket;
             try {
+                // 獲取客戶端的 IP 位址
+                clientIP = socket.getInetAddress().getHostAddress();
                 reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 out = new PrintWriter(new BufferedWriter(
                         new OutputStreamWriter(clientSocket.getOutputStream())), true);
-
-                // 发送保存的消息给新客户端
+                // 發送保存的消息給新客戶端
                 for (String message : messages) {
                     out.println(message);
                 }
@@ -139,13 +233,16 @@ public class socketserverchatroom extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-
         @Override
         public void run() {
             try {
                 while (true) {
                     String tmp = reader.readLine();
                     if (tmp == null) {
+                        reader.close();
+                        out.close();
+                        clientSocket.close();
+                        removeClient(this); // 刪除客戶端
                         break; // 客户端断开连接
                     }
 
@@ -155,20 +252,23 @@ public class socketserverchatroom extends AppCompatActivity {
                         final String senderName = tmp.substring(0, colonIndex);
                         final String message = tmp.substring(colonIndex + 2); // 加 2 是为了跳过冒号和空格
 
+                        // 在訊息前添加客戶端 IP
+                        final String fullMessage = senderName + ": " + message;
+
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                tvMessages.append("收 " + senderName + ": " + message + "\n");
+                                tvMessages.append(fullMessage + "\n");
                             }
                         });
 
                         // 将消息保存到列表中
-                        messages.add(tmp);
+                        messages.add(fullMessage);
 
                         // 将消息广播给其他客户端
                         for (ClientHandler client : clients) {
                             if (client != this) {
-                                client.out.println(tmp);
+                                client.out.println(fullMessage);
                             }
                         }
                     }
@@ -182,62 +282,45 @@ public class socketserverchatroom extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-    }
+}
 
-
-
-//    class SendData implements Runnable {
-//        private String message;
-//
-//        SendData(String message) {
-//            this.message = message;
-//        }
-//
-//        @Override
-//        public void run() {
-//            if (out != null) {
-//                out.println(message);
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        tvMessages.append("發 " + socket.getLocalAddress().getHostAddress()
-//                                + ": " + message + "\n");
-//                    }
-//                });
-//            } else {
-//                Log.e("senderror",message);// 处理 out 为空的情况，可能需要日志记录或其他操作
-//            }
-//        }
-//    }
 
     class SendData implements Runnable {
-        private String message;
+    private String message;
 
+    SendData(String message) {
+        this.message = message;
+    }
 
-        SendData(String message) {
-            this.message = message;
-        }
-
-        @Override
-        public void run() {
-            for (ClientHandler client : clients) {
-                try {
-                    PrintWriter out = new PrintWriter(new BufferedWriter(
-                            new OutputStreamWriter(client.clientSocket.getOutputStream())), true);
-                    out.println(message);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            tvMessages.append("發 " + client.clientSocket.getLocalAddress().getHostAddress()
-                                    + ":"+ message + "\n");
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    @Override
+    public void run() {
+        for (ClientHandler client : clients) {
+            try {
+                PrintWriter out = new PrintWriter(new BufferedWriter(
+                        new OutputStreamWriter(client.clientSocket.getOutputStream())), true);
+                out.println(message);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+        // 在伺服器端處理消息
+        handleServerMessage(message);
+
     }
+
+
+
+}
+    // 在這裡處理伺服器發送的消息，例如顯示在伺服器的 UI 上
+    private void handleServerMessage(String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tvMessages.append( message + "\n");
+            }
+        });
+    }
+
 
 
     private String getLocalIpAddress() throws UnknownHostException {
